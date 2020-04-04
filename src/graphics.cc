37847f48ee12a0479db127a5d76288a9df6e8ee8
Fahien <sir.fahien@gmail.com>
2020-04-04 21:26:24 +0200

    Fix shader file loading

--- src/graphics.cc
+++ src/graphics.cc
@@ -555,10 +555,10 @@ ShaderModule::ShaderModule( Device& d, const std::filesystem::path& path )
 {
 	VkShaderModuleCreateInfo info = {};
 	info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-	info.codeSize = std::filesystem::file_size(path);
+	info.codeSize = std::filesystem::file_size( path );
 
 	std::vector<char> code( info.codeSize );
-	std::ifstream( path ).read( code.data(), info.codeSize );
+	std::ifstream( path, std::ios::binary ).read( code.data(), info.codeSize );
 	info.pCode = reinterpret_cast<uint32_t*>( code.data() );
 
 	const auto res = vkCreateShaderModule(device.handle, &info, nullptr, &handle);
